adfea61df41bbeca79fa2ce4b0b17ed4da241b23
hector4213 <hectorpwns@gmail.com>
2021-02-15 15:38:16 -0500

    add axios dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import covidService from './api/covid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const apiData = await covidService.getData();
+      setData(apiData);
+    };
+    fetchApi();
+    console.log(data);
+  }, []);
+
+  return <h1>Hello World</h1>;
+};
 
 export default App;
