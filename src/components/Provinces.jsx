130a0739268b001c941f1cd25eb7e5c272520596
hector4213 <hectorpwns@gmail.com>
2021-02-16 14:01:32 -0500

    card grid for province data selection

--- src/components/Provinces.jsx
+++ src/components/Provinces.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ProvinceCard from './ProvinceCard';
+import ProvinceDash from './ProvinceDash';
 import AB from '../assets/provinces/ab.png';
 import BC from '../assets/provinces/bc.png';
 import MB from '../assets/provinces/mb.png';
@@ -16,31 +18,43 @@ import YT from '../assets/provinces/yt.png';
 import SK from '../assets/provinces/sk.png';
 
 const PROVINCES = [
-  { name: 'Alberta', img: AB },
-  { name: 'Manitoba', img: MB },
-  { name: 'Ontario', img: ON },
-  { name: 'PEI', img: PEI },
-  { name: 'BC', img: BC },
-  { name: 'New Brunswick', img: NB },
-  { name: 'NL', img: NL },
-  { name: 'Quebec', img: QC },
-  { name: 'Nunavut', img: NV },
-  { name: 'NWT', img: NWT },
-  { name: 'Saskatchewan', img: SK },
-  { name: 'Nova Scotia', img: NS },
-  { name: 'Yukon', img: YT },
+  { name: 'Alberta', img: AB, path: '/alberta' },
+  { name: 'Manitoba', img: MB, path: '/manitoba' },
+  { name: 'Ontario', img: ON, path: '/ontario' },
+  { name: 'PEI', img: PEI, path: '/pei' },
+  { name: 'BC', img: BC, path: '/bc' },
+  { name: 'New Brunswick', img: NB, path: '/nb' },
+  { name: 'NL', img: NL, path: '/nl' },
+  { name: 'Quebec', img: QC, path: '/qc' },
+  { name: 'Nunavut', img: NV, path: '/nunavut' },
+  { name: 'NWT', img: NWT, path: '/nwt' },
+  { name: 'Saskatchewan', img: SK, path: '/saskatchewan' },
+  { name: 'Nova Scotia', img: NS, path: '/novascotia' },
+  { name: 'Yukon', img: YT, path: '/yukon' },
 ];
 
 const Provinces = ({ provinceData }) => {
   return (
     <div className="mt-8 grid lg:grid-cols-4 gap-10">
-      {PROVINCES.map((prov) => (
-        <ProvinceCard
-          name={prov.name}
-          logo={prov.img}
-          province={provinceData.find((item) => item.province === prov.name)}
-        />
-      ))}
+      <Switch>
+        <Route exact path="/">
+          {PROVINCES.map((prov) => (
+            <ProvinceCard
+              name={prov.name}
+              logo={prov.img}
+              province={provinceData.find(
+                (item) => item.province === prov.name,
+              )}
+              path={prov.path}
+            />
+          ))}
+        </Route>
+        {PROVINCES.map((prov) => (
+          <Route path={prov.path}>
+            <ProvinceDash name={prov.name} />
+          </Route>
+        ))}
+      </Switch>
     </div>
   );
 };
