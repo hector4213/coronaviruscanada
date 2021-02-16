43912b6805ffee0a5f1ef0a5f8c682cc523624c3
hector4213 <hectorpwns@gmail.com>
2021-02-16 07:26:05 -0500

    add loading state

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import covidService from './api/covid';
 import Header from './components/Header';
+import Provinces from './components/Provinces';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchApi = async () => {
       const apiData = await covidService.getData();
       setData(apiData);
+      setIsLoading(false);
     };
     fetchApi();
     console.log(data);
   }, []);
 
-  return <Header />;
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Header />
+      <Provinces provinceData={data} />
+    </div>
+  );
 };
 
 export default App;
