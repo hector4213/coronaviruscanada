27e46d26965cd4152c7f714367eb2197392ae427
hector4213 <hectorpwns@gmail.com>
2021-02-16 14:00:10 -0500

    change api function to more descriptive

--- src/api/covid.js
+++ src/api/covid.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const getData = async () => {
+const getSummaries = async () => {
   const response = await axios.get('https://api.opencovid.ca/summary');
   const { summary } = response.data;
   return summary;
 };
 
 export default {
-  getData,
+  getSummaries,
 };
