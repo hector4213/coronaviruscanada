170097a72d0256189f20437610e6d9ee1c41b8a4
hector4213 <hectorpwns@gmail.com>
2021-02-17 13:13:29 -0500

    update camelcase rule"

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     node: true,
   },
   rules: {
+    "camelcase": ["off"], //fix this fkn camel case shit, start province details and a selection on individual health regions
     'jsx-a11y/href-no-hash': ['off'],
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
     'react/react-in-jsx-scope': ['off'],
