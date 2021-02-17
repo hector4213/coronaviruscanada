870bb24a1a5e3db2141a03c271730fba2a5a078e
hector4213 <hectorpwns@gmail.com>
2021-02-16 21:49:48 -0500

    refactor to redux

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import covidService from './api/covid';
 import Header from './components/Header';
 import Provinces from './components/Provinces';
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchApi = async () => {
-      const apiData = await covidService.getSummaries();
-      setData(apiData);
-      setIsLoading(false);
-    };
-    fetchApi();
-    console.log(data);
-  }, []);
-
-  if (isLoading) {
-    return null;
-  }
-
   return (
     <div>
       <Router>
         <Header />
-        <Provinces provinceData={data} />
+        <Provinces />
       </Router>
     </div>
   );
