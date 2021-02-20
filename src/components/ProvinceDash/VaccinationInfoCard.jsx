0912fc1fa516dee619d90ceccfa32b4103bf6d24
hector4213 <hectorpwns@gmail.com>
2021-02-20 15:45:52 -0500

    add props and use prop types

--- src/components/ProvinceDash/VaccinationInfoCard.jsx
+++ src/components/ProvinceDash/VaccinationInfoCard.jsx
@@ -1,12 +1,14 @@
-const VaccinationInfoCard = () => {
+import PropTypes from 'prop-types';
+
+const VaccinationInfoCard = ({ vaccinesToday, totalVaccines }) => {
   return (
     <div className="md:p-8 p-6 mt-5 bg-white shadow-xl rounded-lg flex justify-between dark:bg-gray-800 md:items-center md:flex-row gap-12">
       <div>
         <span className="text-bold text-gray-700 dark:text-gray-400 block">
-          Cumulative Vaccinations
+          Total Vaccinations
         </span>
         <span className="text-green-500 text-4xl md:text-5xl mt-2 font-black block">
-          99999
+          {`${totalVaccines}`}
         </span>
       </div>
       <div className="self-end">
@@ -22,7 +24,7 @@ const VaccinationInfoCard = () => {
             >
               <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z" />
             </svg>
-            Today
+            {`${vaccinesToday}`} Administered Today
           </p>
         </div>
         <p className="text-lg text-gray-600 md:text-right text-left dark:text-gray-400 md:block inline-block md:mb-0">
@@ -31,6 +33,11 @@ const VaccinationInfoCard = () => {
       </div>
     </div>
   );
+};
+
+VaccinationInfoCard.propTypes = {
+  vaccinesToday: PropTypes.number.isRequired,
+  totalVaccines: PropTypes.number.isRequired,
 };
 
 export default VaccinationInfoCard;
