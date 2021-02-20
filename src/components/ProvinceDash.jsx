545e31af99ccc004ea5db361de53789aaa3c46b0
hector4213 <hectorpwns@gmail.com>
2021-02-20 14:20:06 -0500

    refactor active cases to component

--- src/components/ProvinceDash.jsx
+++ src/components/ProvinceDash.jsx
@@ -1,52 +1,19 @@
 import PropTypes from 'prop-types';
-import HealthRegionList from './HealthRegionList';
+import ActiveCasesCard from './ActiveCasesCard';
 
-const ProvinceDash = ({ soleProvince }) => {
-  const getChangePercent = () => {
-    const { active_cases, active_cases_change } = soleProvince;
-    const increase = active_cases - active_cases_change;
-    const percentage = 100 - (active_cases / increase) * 100;
-    const output = `${Math.round(percentage)} %`;
-    return output;
-  };
+import VaccinationInfoCard from './VaccinationInfoCard';
 
+const ProvinceDash = ({ selectedProvince }) => {
   return (
-    <div className="md:p-8 p-6 bg-white shadow-xl rounded-lg flex justify-between dark:bg-gray-800 md:items-center md:flex-row flex-col gap-12">
-      <div>
-        <span className="text-bold text-gray-700 dark:text-gray-400 block">
-          Active Cases
-        </span>
-        <span className="text-yellow-500 text-4xl md:text-5xl mt-2 font-black block">
-          {soleProvince.active_cases}
-        </span>
-      </div>
-      <div className="self-end">
-        <div className="md:text-right text-left md:block">
-          <p className="text-xl md:mb-2 mb-0 dark:text-gray-100 flex items-center increase">
-            <svg
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="h-6 w-6 text-red-500 mr-2"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z" />
-            </svg>
-            {getChangePercent()} increase
-          </p>
-        </div>
-        <p className="text-lg text-gray-600 md:text-right text-left dark:text-gray-400 md:block inline-block md:mb-0">
-          changes from yesterday ({soleProvince.active_cases_change})
-        </p>
-      </div>
-      <HealthRegionList province={soleProvince.province} />
+    <div className="container mx-auto">
+      <ActiveCasesCard selectedProvince={selectedProvince} />
+      <VaccinationInfoCard />
     </div>
   );
 };
 
 ProvinceDash.propTypes = {
-  soleProvince: PropTypes.instanceOf(Object).isRequired,
+  selectedProvince: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default ProvinceDash;
