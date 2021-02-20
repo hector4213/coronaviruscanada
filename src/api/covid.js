ee7938f9bbd6a8e10267371146b4bbc1df61ffc2
hector4213 <hectorpwns@gmail.com>
2021-02-20 14:51:40 -0500

    api call for province specific data

--- src/api/covid.js
+++ src/api/covid.js
@@ -1,11 +1,24 @@
 import axios from 'axios';
 
+const baseURL = 'https://api.opencovid.ca/summary';
+
 const getSummaries = async () => {
-  const response = await axios.get('https://api.opencovid.ca/summary');
+  const response = await axios.get(baseURL);
   const { summary } = response.data;
   return summary;
 };
 
+const getProvinceStats = async (provinceCode) => {
+  const response = await axios.get(baseURL, {
+    params: {
+      loc: provinceCode,
+    },
+  });
+  const { provinceData } = response.data;
+  return provinceData;
+};
+
 export default {
   getSummaries,
+  getProvinceStats,
 };
