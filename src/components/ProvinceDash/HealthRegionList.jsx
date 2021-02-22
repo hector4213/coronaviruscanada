6f83e73dae3e0fc860301f51d2c9f109067b6c96
hector4213 <hectorpwns@gmail.com>
2021-02-22 05:59:05 -0500

    use redux state

--- src/components/ProvinceDash/HealthRegionList.jsx
+++ src/components/ProvinceDash/HealthRegionList.jsx
@@ -1,17 +1,34 @@
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRegionSummary } from '../../redux/ducks/regions';
 import regions from '../../assets/static/healthregions';
+import RegionCard from './RegionCard';
 
 const HealthRegionList = ({ province }) => {
-  console.log(regions, province);
+  const provinceRegion = useSelector((state) => state.regionSummaries);
+  const { hasSelected, regionData } = provinceRegion;
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    dispatch(getRegionSummary(e.target.value));
+  };
+
+  console.log(regionData);
   return (
-    <select
-      className="block w-52 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
-      name="regions"
-    >
-      {regions[province].map((prov) => (
-        <option value={prov.code}>{prov.region}</option>
-      ))}
-    </select>
+    <>
+      <label className="text-gray-700" htmlFor="regions">
+        Select to view region summary
+        <select
+          onChange={handleChange}
+          className="block w-52 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
+          name="regions"
+        >
+          {regions[province].map((prov) => (
+            <option value={prov.code}>{prov.region}</option>
+          ))}
+        </select>
+      </label>
+      {hasSelected && <RegionCard data={regionData} />}
+    </>
   );
 };
 
