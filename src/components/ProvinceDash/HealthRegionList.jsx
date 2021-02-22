4e6b083d79bcb4724401fcebd8037edbafa22556
hector4213 <hectorpwns@gmail.com>
2021-02-22 03:22:43 -0500

    change to select component

--- src/components/ProvinceDash/HealthRegionList.jsx
+++ src/components/ProvinceDash/HealthRegionList.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
 import regions from '../../assets/static/healthregions';
-import HealthRegionItem from './HealthRegionItem';
 
 const HealthRegionList = ({ province }) => {
   console.log(regions, province);
   return (
-    <div className="container flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow">
-      <ul className="flex flex-col divide divide-y">
-        {regions[province].map((reg) => (
-          <HealthRegionItem region={reg.region} />
-        ))}
-      </ul>
-    </div>
+    <select
+      className="block w-52 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
+      name="regions"
+    >
+      {regions[province].map((prov) => (
+        <option value={prov.code}>{prov.region}</option>
+      ))}
+    </select>
   );
 };
 
