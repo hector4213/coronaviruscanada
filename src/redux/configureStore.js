8854c87b5dee295936e5784ce0a00cec4216232d
hector4213 <hectorpwns@gmail.com>
2021-02-22 04:14:37 -0500

    add reducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import summaryReducer from './ducks/summary';
+import regionReducer from './ducks/regions';
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -11,6 +12,7 @@ const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const reducers = combineReducers({
   summaries: summaryReducer,
+  regionSummaries: regionReducer,
 });
 
 const store = createStore(reducers, enhancer);
