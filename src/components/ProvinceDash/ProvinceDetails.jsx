e7d3bd6021de1e098a43ec55cc7bd24236216280
hector4213 <hectorpwns@gmail.com>
2021-02-23 23:54:18 -0500

    function to convert date

--- src/components/ProvinceDash/ProvinceDetails.jsx
+++ src/components/ProvinceDash/ProvinceDetails.jsx
@@ -2,13 +2,19 @@ import PropTypes from 'prop-types';
 import HealthRegionList from './HealthRegionList';
 
 const ProvinceDetails = ({ province, date }) => {
+  const convertDate = (dateString) => {
+    const formattedDate = dateString.split('-').reverse().join('-');
+    const outputDate = new Date(formattedDate).toDateString();
+    return outputDate;
+  };
+
   return (
-    <div className="md:p-8 p-6 mt-5 bg-white shadow-xl rounded-lg flex justify-center dark:bg-gray-800 md:items-center md:flex-row gap-12">
-      <h2 className="text-4xl font-extrabold text-black dark:text-white sm:text-4xl">
+    <div className="flex-col md:p-8 p-6 mt-5 bg-white shadow-xl rounded-lg flex justify-center dark:bg-gray-800 md:items-center md:flex-row gap-12">
+      <h2 className="text-3xl font-bold text-black dark:text-white sm:text-4xl text-center">
         <span className="block">{province}</span>
       </h2>
-      <h2 className="text-4xl font-extrabold text-black dark:text-white sm:text-4xl">
-        <span className="block">{date}</span>
+      <h2 className="text-2xl font-bold text-black dark:text-white sm:text-4xl text-center">
+        <span className="block">{convertDate(date)}</span>
       </h2>
       <HealthRegionList province={province} />
     </div>
