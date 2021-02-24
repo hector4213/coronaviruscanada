1bdf1f8c9823ad5cc695eff459dc07eaf9d76008
hector4213 <hectorpwns@gmail.com>
2021-02-23 23:24:55 -0500

    remove as block

--- src/components/ProvinceDash/HealthRegionList.jsx
+++ src/components/ProvinceDash/HealthRegionList.jsx
@@ -19,7 +19,7 @@ const HealthRegionList = ({ province }) => {
         Select to view region summary
         <select
           onChange={handleChange}
-          className="block w-52 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
+          className="text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
           name="regions"
         >
           {regions[province].map((prov) => (
