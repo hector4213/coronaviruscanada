7b50a968553cface9dbd01ff85cc8151a4ce2eb3
hector4213 <hectorpwns@gmail.com>
2021-02-27 21:49:43 -0500

    separated state for province grid, dashboard, and region card

--- src/redux/ducks/summary.js
+++ src/redux/ducks/summary.js
@@ -5,6 +5,8 @@ const DATA_SUCCESS = 'DATA_SUCCESS';
 const DATA_ERROR = 'DATA_ERROR';
 const CHANGE_DATE = 'CHANGE_DATE';
 const SET_TODAY_DATE = 'SET_TODAY_DATE';
+const FETCH_TODAY = 'FETCH_TODAY';
+const FETCH_PROVINCE = 'FETCH_PROVINCE';
 
 export const fetchSummaries = (summaries) => ({
   type: FETCH_SUMMARIES,
@@ -30,16 +32,28 @@ export const setTodayDate = (currDate) => ({
   payload: currDate,
 });
 
+export const fetchTodaySummaries = (summaries) => ({
+  type: FETCH_TODAY,
+  payload: summaries,
+});
+
+export const fetchProvinceData = (summary) => ({
+  type: FETCH_PROVINCE,
+  payload: summary,
+});
+
 const initialState = {
   summaries: [],
+  currentProvince: '',
   isLoading: true,
   currentDate: '',
+  today: [],
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_SUMMARIES: {
-      return { ...state, summaries: action.payload };
+    case FETCH_PROVINCE: {
+      return { ...state, currentProvince: action.payload };
     }
     case DATA_SUCCESS: {
       return { ...state, isLoading: action.payload };
@@ -53,28 +67,41 @@ export default (state = initialState, action) => {
     case SET_TODAY_DATE: {
       return { ...state, currentDate: action.payload };
     }
+    case FETCH_TODAY: {
+      return { ...state, today: action.payload };
+    }
     default:
       return state;
   }
 };
 
-export const getSummaries = () => async (dispatch) => {
+export const changeSummaryDate = (userDate) => async (dispatch) => {
   try {
-    const summaries = await covidService.getSummaries();
-    dispatch(fetchSummaries(summaries));
+    const specificSummary = await covidService.getSummaryByDate(userDate);
+    dispatch(userChangedDate(specificSummary));
     dispatch(dataLoaded());
   } catch (error) {
     dispatch(dataError());
   }
 };
 
-export const changeSummaryDate = (provCode, userDate) => async (dispatch) => {
+export const fetchTodaySummary = () => async (dispatch) => {
   try {
-    const specificSummary = await covidService.getSummaryByDate(
-      provCode,
-      userDate,
-    );
-    dispatch(userChangedDate(specificSummary));
+    const todayData = await covidService.getSummaries();
+    dispatch(fetchTodaySummaries(todayData));
+    dispatch(dataLoaded());
+  } catch (error) {
+    dispatch(dataError());
+  }
+};
+
+export const fetchProvinceSummary = (provCode, userDate) => async (
+  dispatch,
+) => {
+  try {
+    const summary = await covidService.getProvinceSummary(provCode, userDate);
+    dispatch(fetchProvinceData(summary));
+    dispatch(setTodayDate(summary.date.split('-').reverse().join('-')));
   } catch (error) {
     dispatch(dataError());
   }
