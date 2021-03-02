6ce4816308a724ca986a4779bdfb17989a0c4698
hector4213 <hectorpwns@gmail.com>
2021-03-01 21:27:02 -0500

    add actions for unmounting component

--- src/redux/ducks/regions.js
+++ src/redux/ducks/regions.js
@@ -3,6 +3,7 @@ import covidService from '../../api/covid';
 const FETCH_REGION_SUMMARY = 'FETCH_REGION_SUMMARY';
 const HAS_SELECTED = 'HAS_SELECTED';
 const CLEAR_SELECTION = 'CLEAR_SELECTION';
+const REGION_SELECT = 'REGION_SELECT';
 
 export const fetchRegionData = (regionSummary) => ({
   type: FETCH_REGION_SUMMARY,
@@ -14,14 +15,20 @@ export const clearRegionSummary = () => ({
   payload: false,
 });
 
-export const hasSelected = () => ({
+export const userHasSelected = (bool) => ({
   type: HAS_SELECTED,
-  payload: true,
+  payload: bool,
+});
+
+export const regionSelect = (regionCode) => ({
+  type: REGION_SELECT,
+  payload: regionCode,
 });
 
 const initialState = {
   regionData: null,
   hasSelected: false,
+  selectedRegion: null,
 };
 
 export default (state = initialState, action) => {
@@ -35,20 +42,23 @@ export default (state = initialState, action) => {
     case HAS_SELECTED: {
       return { ...state, hasSelected: action.payload };
     }
+    case REGION_SELECT: {
+      return { ...state, selectedRegion: action.payload };
+    }
     default: {
       return state;
     }
   }
 };
 
-export const getRegionSummary = (region, userDate = '') => async (dispatch) => {
+export const getRegionSummary = (region, userDate) => async (dispatch) => {
   try {
     const regionSummaries = await covidService.getRegionSummary(
       region,
       userDate,
     );
     dispatch(fetchRegionData(regionSummaries));
-    dispatch(hasSelected());
+    dispatch(userHasSelected(true));
   } catch (error) {
     console.log(error);
   }
