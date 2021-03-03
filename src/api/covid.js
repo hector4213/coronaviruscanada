1cf24e5c59b204f7930326544d783eb0e6bb2546
hector4213 <hectorpwns@gmail.com>
2021-03-02 22:35:16 -0500

    add api call to get api version date

--- src/api/covid.js
+++ src/api/covid.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseURL = 'https://api.opencovid.ca/summary';
+const versionURL = 'https://api.opencovid.ca/version';
 
 const getSummaries = async () => {
   const response = await axios.get(baseURL);
@@ -40,9 +41,16 @@ const getProvinceSummary = async (provCode, userDate = '') => {
   return provinceSummary;
 };
 
+const getAPIVersion = async () => {
+  const response = await axios.get(versionURL);
+  const { version } = response.data;
+  const date = version.substring(0, 10);
+  return date;
+};
 export default {
   getSummaries,
   getRegionSummary,
   getSummaryByDate,
   getProvinceSummary,
+  getAPIVersion,
 };
