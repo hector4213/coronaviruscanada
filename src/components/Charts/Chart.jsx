09090b030f3ef83e422826e17b449a897c3787d5
hector4213 <hectorpwns@gmail.com>
2021-03-06 22:18:20 -0500

    test chart

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
 
 const Chart = () => {
-  return <div></div>;
+  const [chartData, setChartData] = useState({});
+
+  const chart = async () => {
+    const dataCases = [];
+    const response = await fetch(
+      'https://api.opencovid.ca/timeseries?stat=cases&loc=prov&date=03-03-2021',
+    );
+    const apiData = await response.json();
+
+    apiData.cases.forEach((item) => dataCases.push(item));
+
+    setChartData({
+      labels: apiData.cases.map((item) => item.province),
+      datasets: [
+        {
+          label: 'Active Cases',
+          data: dataCases.map((item) => item.cases),
+        },
+      ],
+    });
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+  return (
+    <div>
+      <Bar data={chartData} width={100} height={50} options={{}} />
+    </div>
+  );
 };
 
 export default Chart;
