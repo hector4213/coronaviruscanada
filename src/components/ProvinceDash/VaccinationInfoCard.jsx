14409e4c47264f63a14e55dde22f659edf74211c
hector4213 <hectorpwns@gmail.com>
2021-03-07 23:09:12 -0500

    change icon

--- src/components/ProvinceDash/VaccinationInfoCard.jsx
+++ src/components/ProvinceDash/VaccinationInfoCard.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { GiTripleNeedle } from 'react-icons/gi';
 
 const VaccinationInfoCard = ({ vaccinesToday, totalVaccines }) => {
   return (
@@ -14,22 +15,10 @@ const VaccinationInfoCard = ({ vaccinesToday, totalVaccines }) => {
       <div className="self-end">
         <div className="md:text-right text-left md:block">
           <p className="text-xl md:mb-2 mb-0 dark:text-gray-100 flex items-center increase">
-            <svg
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="h-6 w-6 text-red-500 mr-2"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z" />
-            </svg>
+            <GiTripleNeedle color="red" size="2.5rem" />
             {`${vaccinesToday}`} Administered Today
           </p>
         </div>
-        <p className="text-lg text-gray-600 md:text-right text-left dark:text-gray-400 md:block inline-block md:mb-0">
-          changes from yesterday
-        </p>
       </div>
     </div>
   );
