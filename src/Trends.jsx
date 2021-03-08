9b5b0ffc02ced8f585d7864c4ef322f8ce951de4
hector4213 <hectorpwns@gmail.com>
2021-03-07 23:17:53 -0500

    add placeholder header, deciding on what data to show

--- src/Trends.jsx
+++ src/Trends.jsx
@@ -3,6 +3,11 @@ import Chart from './components/Charts/Chart';
 const Trends = () => {
   return (
     <div>
+      <div>
+        <h1 className="text-center text-3xl p-6">
+          Cumulative Cases for Canada
+        </h1>
+      </div>
       <Chart />
     </div>
   );
