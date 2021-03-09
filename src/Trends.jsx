d827e0175265c2d318d135dac3cffe3ee006cd08
hector4213 <hectorpwns@gmail.com>
2021-03-09 01:57:52 -0500

    remove space

--- src/Trends.jsx
+++ src/Trends.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProvTrendsByDate } from './redux/ducks/chart';
 import Chart from './components/Charts/Chart';
-
 import ProvinceBtn from './components/Charts/ProvinceBtn';
 
 const Trends = () => {
