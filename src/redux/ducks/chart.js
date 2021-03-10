26662b3ccb06982d14dfdf1f6e79665821d364d1
hector4213 <hectorpwns@gmail.com>
2021-03-10 02:01:35 -0500

    add data for no vaccines using ca pop

--- src/redux/ducks/chart.js
+++ src/redux/ducks/chart.js
@@ -15,9 +15,14 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case SET_VACCINE_DATA: {
       const { payload } = action;
+      const noVaccine = 37589262 - payload[0].cumulative_avaccine;
       return {
         ...state,
-        data: [payload[0].cumulative_avaccine, payload[1].cumulative_cvaccine],
+        data: [
+          payload[0].cumulative_avaccine,
+          payload[1].cumulative_cvaccine,
+          noVaccine,
+        ],
       };
     }
     default: {
