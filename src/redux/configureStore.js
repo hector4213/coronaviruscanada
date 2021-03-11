d95e67bb2d3843a4cc3ab24882478474771081bb
hector4213 <hectorpwns@gmail.com>
2021-03-11 13:41:23 -0500

    add region reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import summarySlice from './ducks/summarySlice';
 import infoSlice from './ducks/infoSlice';
+import regionSlice from './ducks/regionSlice';
 
 export default configureStore({
   reducer: {
     summaries: summarySlice,
+    regionSummaries: regionSlice,
     appData: infoSlice,
   },
 });
