21b1a1047c1f40e8aa619ee753cfbdcf66e893f4
hector4213 <hectorpwns@gmail.com>
2021-03-11 00:15:41 -0500

    refactor store config

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,33 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import summarySlice from './ducks/summary'; //  rename to slice
+import summarySlice from './ducks/summarySlice';
+import infoSlice from './ducks/infoSlice';
 
 export default configureStore({
   reducer: {
     summaries: summarySlice,
+    info: infoSlice,
   },
 });
-
-// import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-
-// import regionReducer from './ducks/regions';
-// import appDataReducer from './ducks/info';
-// import chartReducer from './ducks/chart';
-
-// const composeEnhancers =
-//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-//     : compose;
-
-// const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-// const reducers = combineReducers({
-//   summaries: summaryReducer,
-//   regionSummaries: regionReducer,
-//   appData: appDataReducer,
-//   chart: chartReducer,
-// });
-
-// const store = createStore(reducers, enhancer);
-
-// export default store;
