a71fb79de42d7a84f1b1d9a1424b41b39b4a4a89
hector4213 <hectorpwns@gmail.com>
2021-03-12 15:02:22 -0500

    vaccine chart refactored

--- src/redux/ducks/vaccineChartSlice.js
+++ src/redux/ducks/vaccineChartSlice.js
@@ -8,13 +8,18 @@ export const getVaccineData = createAsyncThunk(
       covidService.getVaccinationData(userDate, 'avaccine'),
       covidService.getVaccinationData(userDate, 'cvaccine'),
     ]);
-    console.log(responses);
-    return responses;
+    const apiData = responses.map(
+      (item) => item.cumulative_avaccine || item.cumulative_cvaccine,
+    );
+    const unVaccinated = 37589262 - apiData[1];
+    const data = apiData.concat(unVaccinated);
+    return data;
   },
 );
 
 const initialState = {
-  data: [],
+  data: null,
+  canPopulation: 37589262,
 };
 
 const vaccineChartSlice = createSlice({
@@ -29,59 +34,3 @@ const vaccineChartSlice = createSlice({
 });
 
 export default vaccineChartSlice.reducer;
-
-// import covidService from '../../api/covid';
-
-// const SET_VACCINE_DATA = 'SET_VACCINE_DATA';
-
-// export const setVaccineData = (data) => ({
-//   type: SET_VACCINE_DATA,
-//   payload: data,
-// });
-
-// const initialState = {
-//   data: [],
-// };
-
-// export default (state = initialState, action) => {
-//   switch (action.type) {
-//     case SET_VACCINE_DATA: {
-//       const { payload } = action;
-//       const noVaccine = 37589262 - payload[0].cumulative_avaccine;
-//       return {
-//         ...state,
-//         data: [
-//           // use immer here
-//           payload[0].cumulative_avaccine,
-//           payload[1].cumulative_cvaccine,
-//           noVaccine,
-//         ],
-//       };
-//     }
-//     default: {
-//       return state;
-//     }
-//   }
-// };
-
-// export const getVaccineStats = (userDate) => async (dispatch) => {
-//   try {
-//     const totalVaccinated = covidService.getVaccinationData(
-//       'canada',
-//       userDate,
-//       'avaccine',
-//     );
-//     const fullyVaccinated = covidService.getVaccinationData(
-//       'canada',
-//       userDate,
-//       'cvaccine',
-//     );
-
-//     const responses = await Promise.all([totalVaccinated, fullyVaccinated]);
-//     console.log(responses);
-
-//     dispatch(setVaccineData(responses));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
