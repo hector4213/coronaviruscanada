09f222758a5a2c57b0c6b4cfd03a0f1d7c0e0940
hector4213 <hectorpwns@gmail.com>
2021-03-11 22:30:12 -0500

    add chart reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import summarySlice from './ducks/summarySlice';
 import infoSlice from './ducks/infoSlice';
 import regionSlice from './ducks/regionSlice';
+import vaccineChartSlice from './ducks/vaccineChartSlice';
 
 export default configureStore({
   reducer: {
     summaries: summarySlice,
     regionSummaries: regionSlice,
     appData: infoSlice,
+    vaccineData: vaccineChartSlice,
   },
 });
