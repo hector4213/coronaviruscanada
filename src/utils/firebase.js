42c4e8d360462de05cf7704f0dd059b2d2a73df7
hector4213 <hectorpwns@gmail.com>
2021-03-16 13:31:12 -0400

    add firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6x_Tmtp99sq9Wi8bFIdA7Y1FJg0xvLZw',
+  authDomain: 'covidcanada-55475.firebaseapp.com',
+  databaseURL: 'https://covidcanada-55475-default-rtdb.firebaseio.com',
+  projectId: 'covidcanada-55475',
+  storageBucket: 'covidcanada-55475.appspot.com',
+  messagingSenderId: '732124170912',
+  appId: '1:732124170912:web:738e3f82cc808cb4d997c5',
+  measurementId: 'G-FCHE2C2YQK',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
