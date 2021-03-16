46fc1d685d9303dd9116fa308f36f3e0eb7208d5
hector4213 <hectorpwns@gmail.com>
2021-03-16 13:45:20 -0400

    use env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyD6x_Tmtp99sq9Wi8bFIdA7Y1FJg0xvLZw',
+  apiKey: process.env.FIREBASE_KEY,
   authDomain: 'covidcanada-55475.firebaseapp.com',
   databaseURL: 'https://covidcanada-55475-default-rtdb.firebaseio.com',
   projectId: 'covidcanada-55475',
   storageBucket: 'covidcanada-55475.appspot.com',
   messagingSenderId: '732124170912',
-  appId: '1:732124170912:web:738e3f82cc808cb4d997c5',
-  measurementId: 'G-FCHE2C2YQK',
+  appId: '1:732124170912:web:7532dc8c337c320ad997c5',
+  measurementId: 'G-M9QE4N7R11',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
