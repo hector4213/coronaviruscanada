c4ac4416270e1c1ccbe7ec8e2b8df3bfab9cd642
hector4213 <hectorpwns@gmail.com>
2021-03-16 21:11:35 -0400

    component uses firebase instead of static data

--- src/Today.jsx
+++ src/Today.jsx
@@ -2,59 +2,37 @@ import React, { useEffect } from 'react';
 import { Route, useRouteMatch } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getTodaySummaries } from './redux/ducks/summarySlice';
+import { getAppProvinces } from './redux/ducks/infoSlice';
 import ProvinceCard from './components/ProvinceCard';
 import ProvinceDash from './components/ProvinceDash';
-import AB from './assets/provinces/ab.png';
-import BC from './assets/provinces/bc.png';
-import MB from './assets/provinces/mb.png';
-import NB from './assets/provinces/nb.png';
-import QC from './assets/provinces/qc.png';
-import NL from './assets/provinces/nl.png';
-import NS from './assets/provinces/ns.png';
-import NV from './assets/provinces/nv.png';
-import ON from './assets/provinces/on.png';
-import PEI from './assets/provinces/pei.png';
-import NWT from './assets/provinces/nwt.png';
-import YT from './assets/provinces/yt.png';
-import SK from './assets/provinces/sk.png';
-
-const PROVINCES = [
-  { id: 1, name: 'Alberta', img: AB, path: '/alberta', code: 'AB' },
-  { id: 2, name: 'Manitoba', img: MB, path: '/manitoba', code: 'MB' },
-  { id: 3, name: 'Ontario', img: ON, path: '/ontario', code: 'ON' },
-  { id: 4, name: 'PEI', img: PEI, path: '/pei', code: 'PE' },
-  { id: 5, name: 'BC', img: BC, path: '/bc', code: 'BC' },
-  { id: 6, name: 'New Brunswick', img: NB, path: '/nb', code: 'NB' },
-  { id: 7, name: 'NL', img: NL, path: '/nl', code: 'NL' },
-  { id: 8, name: 'Quebec', img: QC, path: '/qc', code: 'QC' },
-  { id: 9, name: 'Nunavut', img: NV, path: '/nunavut', code: 'NU' },
-  { id: 10, name: 'NWT', img: NWT, path: '/nwt', code: 'NT' },
-  { id: 11, name: 'Saskatchewan', img: SK, path: '/saskatchewan', code: 'SK' },
-  { id: 12, name: 'Nova Scotia', img: NS, path: '/novascotia', code: 'NS' },
-  { id: 13, name: 'Yukon', img: YT, path: '/yukon', code: 'YT' },
-];
 
 const Provinces = () => {
-  const provinceGrid = useSelector((state) => state.summaries);
+  const { isLoading, today } = useSelector((state) => state.summaries);
+  const { provinceData } = useSelector((state) => state.appData);
   const dispatch = useDispatch();
   const { path } = useRouteMatch();
-  const { isLoading, today } = provinceGrid;
 
   useEffect(() => {
-    if (today.length < 1) {
+    if (!today) {
       dispatch(getTodaySummaries());
+    }
+  }, [today]);
+
+  useEffect(() => {
+    if (provinceData.length < 1) {
+      dispatch(getAppProvinces());
     }
   }, []);
 
   if (isLoading) {
     return null; //   loader here state is false if data loaded
   }
-
+  console.log(provinceData);
   return (
     <div className="container mt-5 p-5 mx-auto">
-      <div className="md:grid md:grid-cols-3 lg:grid-cols-4">
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-3">
         <Route exact path={`${path}`}>
-          {PROVINCES.map((prov) => (
+          {provinceData.map((prov) => (
             <ProvinceCard
               key={prov.id}
               name={prov.name}
@@ -65,7 +43,7 @@ const Provinces = () => {
           ))}
         </Route>
       </div>
-      {PROVINCES.map((prov) => (
+      {provinceData.map((prov) => (
         <Route path={`${path}${prov.path}`} key={prov.id}>
           <ProvinceDash code={prov.code} />
         </Route>
