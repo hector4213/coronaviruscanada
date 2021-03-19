5d295df759345362cd04761f8556f03fb1ac1426
hector4213 <hectorpwns@gmail.com>
2021-03-19 07:04:15 -0400

    move stat card to trends

--- src/components/Charts/VaccineChart.jsx
+++ src/components/Charts/VaccineChart.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux';
 import { Pie } from 'react-chartjs-2';
-import CanadaStats from './CanadaStats';
 
 const VaccineChart = () => {
   const { stats } = useSelector((state) => state.vaccineData);
@@ -37,7 +36,6 @@ const VaccineChart = () => {
   };
   return (
     <div className="h-1/2 w-full md:w-full md:h-1/2">
-      <CanadaStats />
       <div className="h-80 w-full bg-white p-6 shadow-xl rounded-lg mt-3">
         <Pie data={data} options={OPTIONS} />
       </div>
