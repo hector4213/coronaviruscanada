56ed9e8b060a9bf3ea375bcfd1b4d979dacfc152
hector4213 <hectorpwns@gmail.com>
2021-03-19 07:03:56 -0400

    move stat card to trends

--- src/Trends.jsx
+++ src/Trends.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getVaccineData } from './redux/ducks/vaccineChartSlice';
 import VaccineChart from './components/Charts/VaccineChart';
 import CasesChart from './components/Charts/CasesChart';
+import CanadaStats from './components/Charts/CanadaStats';
 
 const Trends = () => {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ const Trends = () => {
   return (
     <div className="flex flex-col items-center py-4 px-4 bg-gray-100 lg:flex-row ">
       <div className="w-full my-1 mx-1 md:w-1/2 md:min-h-screen md:mr-5">
+        <CanadaStats />
         <VaccineChart />
       </div>
       <div className="min-h-screen md:flex md:w-1/2">
