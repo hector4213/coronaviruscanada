db9f1610d937e02afe3d0605b9996505ea7dc6f4
hector4213 <hectorpwns@gmail.com>
2021-03-20 08:13:10 -0400

    fix layout for provincechart

--- src/components/Charts/ProvinceChart.jsx
+++ src/components/Charts/ProvinceChart.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { useSelector, useDispatch } from 'react-redux';
 import { getVaccineStats } from '../../redux/ducks/vaccineGapSlice';
 
@@ -23,14 +23,14 @@ const ProvinceChart = () => {
     datasets: [
       {
         label: 'Doses Distributed',
-        fill: false,
+        fill: true,
         backgroundColor: 'rgb(0, 255, 128)',
         borderColor: 'rgba(0, 255, 128, 0.75)',
         data: distributed,
       },
       {
         label: 'Doses Administered',
-        fill: false,
+        fill: true,
         backgroundColor: 'rgb(255, 128, 0)',
         borderColor: 'rgba(255, 128, 0, 0.75)',
         borderDash: [5, 5],
@@ -48,9 +48,11 @@ const ProvinceChart = () => {
     maintainAspectRatio: false,
   };
   return (
-    <div className="h-80  bg-white shadow-xl rounded-lg text-center md:h-1/2 ">
+    <div className="bg-white shadow-xl rounded-lg text-center">
       <input type="date" min={apiStartDate} max={apiLastUpdated} />
-      <Bar data={data} options={OPTIONS} />
+      <div className="h-96 w-80 md:w-full">
+        <Line data={data} options={OPTIONS} />
+      </div>
     </div>
   );
 };
