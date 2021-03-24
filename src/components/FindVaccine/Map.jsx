f3ad9ddcdb941520a964a29549894225becfd8df
hector4213 <hectorpwns@gmail.com>
2021-03-23 23:12:17 -0400

    mapsetup

--- src/components/FindVaccine/Map.jsx
+++ src/components/FindVaccine/Map.jsx
@@ -1,22 +1,30 @@
+import { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
-import PropTypes from 'prop-types';
 
-const Map = ({ viewport }) => {
+const Map = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 43.6532,
+    longitude: -79.3832,
+    zoom: 10,
+    width: '100vw',
+    height: '100vh',
+  });
+
   return (
     <>
       <ReactMapGL
         // eslint-disable-next-line
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        mapStyle="mapbox://styles/hector4213/ckmmsc0hm2mu717pjrug3glra"
+        onViewportChange={(vp) => {
+          setViewport(vp);
+        }}
       >
         markers here
       </ReactMapGL>
     </>
   );
-};
-
-Map.propTypes = {
-  viewport: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Map;
