039ad6e941d0abb41d39a689a44a8ebfa717f6fd
hector4213 <hectorpwns@gmail.com>
2021-03-29 22:10:06 -0400

    start using map component

--- src/components/FindVaccine/Map.jsx
+++ src/components/FindVaccine/Map.jsx
@@ -1,29 +1,23 @@
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
-  const [viewport, setViewport] = useState({
-    latitude: 43.6532,
-    longitude: -79.3832,
-    zoom: 10,
-    width: '100vw',
-    height: '100vh',
-  });
-
   return (
-    <>
-      <ReactMapGL
-        // eslint-disable-next-line
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-        mapStyle="mapbox://styles/hector4213/ckmmsc0hm2mu717pjrug3glra"
-        onViewportChange={(vp) => {
-          setViewport(vp);
-        }}
-      >
-        markers here
-      </ReactMapGL>
-    </>
+    <MapContainer
+      className="h-full w-full"
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGVjdG9yNDIxMyIsImEiOiJja21tcmYzbTkwNDhiMm9td2JrNHRzejc0In0.XmU3r47TYhsm9wbLyS1qJA"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
