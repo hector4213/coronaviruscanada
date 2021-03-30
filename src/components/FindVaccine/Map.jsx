85d238849ef51503570c93706c9f1ea494447f60
hector4213 <hectorpwns@gmail.com>
2021-03-30 14:40:26 -0400

    receive userLocation prop, prop-types

--- src/components/FindVaccine/Map.jsx
+++ src/components/FindVaccine/Map.jsx
@@ -1,24 +1,30 @@
+import PropTypes from 'prop-types';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const Map = () => {
+const Map = ({ userLocation }) => {
+  const { lat, long } = userLocation;
   return (
     <MapContainer
       className="h-full w-full"
-      center={[51.505, -0.09]}
+      center={[lat, long]}
       zoom={13}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGVjdG9yNDIxMyIsImEiOiJja21tcmYzbTkwNDhiMm9td2JrNHRzejc0In0.XmU3r47TYhsm9wbLyS1qJA"
+        url={`https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX}`}
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[lat, long]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
     </MapContainer>
   );
+};
+
+Map.propTypes = {
+  userLocation: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Map;
