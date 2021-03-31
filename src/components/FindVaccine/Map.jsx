82341ead7ea52dca82bfd4d92ff4332d1ed9c029
hector4213 <hectorpwns@gmail.com>
2021-03-30 23:07:17 -0400

    use array instead for user location

--- src/components/FindVaccine/Map.jsx
+++ src/components/FindVaccine/Map.jsx
@@ -1,30 +1,34 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import MyComponent from './MyComponent';
 
 const Map = ({ userLocation }) => {
-  const { lat, long } = userLocation;
+  useEffect(() => {}, []);
+
   return (
     <MapContainer
       className="h-full w-full"
-      center={[lat, long]}
+      center={userLocation}
       zoom={13}
       scrollWheelZoom={false}
     >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="http://osm.org/copyright">Mapbox</a> contributors'
         url={`https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX}`}
       />
-      <Marker position={[lat, long]}>
+      <Marker position={userLocation}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+      <MyComponent location={userLocation} />
     </MapContainer>
   );
 };
 
 Map.propTypes = {
-  userLocation: PropTypes.instanceOf(Object).isRequired,
+  userLocation: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default Map;
