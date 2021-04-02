e64861014dd1616221c8674285a5b1630abcb7af
hector4213 <hectorpwns@gmail.com>
2021-04-02 14:16:24 -0400

    add thunk to search for local hospitals

--- src/redux/ducks/mapSlice.js
+++ src/redux/ducks/mapSlice.js
@@ -1,6 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import searchService from '../../api/search';
 
 // Thunk here for search API
+export const getLocalHospitals = createAsyncThunk(
+  'map/getLocalHospitals',
+  async (coords) => {
+    const results = await searchService.findHospitals(coords);
+    return results;
+  },
+);
 
 const initialState = {
   userLocation: [43.6447778, -79.398395],
@@ -13,6 +21,11 @@ const mapSlice = createSlice({
   reducers: {
     setLocation: (state, { payload }) => {
       state.userLocation = [...payload];
+    },
+  },
+  extraReducers: {
+    [getLocalHospitals.fulfilled]: (state, { payload }) => {
+      state.mapResults = [...payload];
     },
   },
 });
