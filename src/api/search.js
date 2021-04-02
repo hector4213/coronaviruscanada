555d56bfc566696a9766e3dc229276c0b2c8b258
hector4213 <hectorpwns@gmail.com>
2021-04-02 14:16:08 -0400

    api call to use coordinates

--- src/api/search.js
+++ src/api/search.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 const baseURL = 'https://www.mapquestapi.com/search/v2/radius';
 
 const findHospitals = async (origin) => {
+  const [lat, long] = origin;
   const response = await axios.get(baseURL, {
     params: {
-      origin,
+      origin: `${lat}, ${long}`,
       radius: 50, // maybe let user be able to choose
       maxMatches: 10,
       ambiguities: 'ignore',
