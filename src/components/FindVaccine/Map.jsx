1129e4fc9bc9f6419ee6de27f5fb5af4342b120f
hector4213 <hectorpwns@gmail.com>
2021-04-02 21:11:26 -0400

    use hospital marker

--- src/components/FindVaccine/Map.jsx
+++ src/components/FindVaccine/Map.jsx
@@ -1,34 +1,39 @@
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import MyComponent from './MyComponent';
+import HospitalMarker from './HospitalMarker';
+import UserMarker from './UserMarker';
 
-const Map = ({ userLocation }) => {
-  useEffect(() => {}, []);
-
+const Map = ({ userLocation, mapResults }) => {
   return (
     <MapContainer
       className="h-full w-full"
       center={userLocation}
       zoom={13}
-      scrollWheelZoom={false}
+      scrollWheelZoom
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">Mapbox</a> contributors'
         url={`https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX}`}
       />
       <Marker position={userLocation}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+        <Popup>You are here :)</Popup>
       </Marker>
-      <MyComponent location={userLocation} />
+      <UserMarker location={userLocation} />
+      {mapResults.length < 1
+        ? null
+        : mapResults.map((hospital) => (
+            <HospitalMarker
+              hospital={hospital}
+              position={[hospital.fields.lat, hospital.fields.lng]}
+            />
+          ))}
     </MapContainer>
   );
 };
 
 Map.propTypes = {
   userLocation: PropTypes.instanceOf(Array).isRequired,
+  mapResults: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default Map;
