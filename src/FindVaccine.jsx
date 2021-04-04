f6e22161a176b498839bfb429b9590485e716b0e
hector4213 <hectorpwns@gmail.com>
2021-04-03 21:47:00 -0400

    use modal, hospital list

--- src/FindVaccine.jsx
+++ src/FindVaccine.jsx
@@ -3,9 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getLocalHospitals } from './redux/ducks/mapSlice';
 
 import Map from './components/FindVaccine/Map';
+import AppointmentModal from './components/FindVaccine/AppointmentModal';
+import HospitalList from './components/FindVaccine/HospitalList';
 
 const FindVaccine = () => {
-  const { userLocation, mapResults } = useSelector((state) => state.map);
+  const { userLocation, mapResults, isModalOpen } = useSelector(
+    (state) => state.map,
+  );
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,15 +21,14 @@ const FindVaccine = () => {
 
   return (
     <div className="mt-4 md:flex flex-row w-screen">
-      <div className="h-96 max-w-sm p-4 shadow-xl rounded-lg md:max-w-xl md:flex-1">
+      <div className="h-96 max-w-sm z-30 p-4 shadow-xl rounded-lg md:max-w-xl md:flex-1">
         {userLocation.length < 0 ? null : (
           <Map userLocation={userLocation} mapResults={mapResults} />
         )}
       </div>
       <div className="p-4">
-        {mapResults.map((hospital) => (
-          <p>{hospital.name}</p>
-        ))}
+        <HospitalList hospitals={mapResults} />
+        {isModalOpen && <AppointmentModal />}
       </div>
     </div>
   );
