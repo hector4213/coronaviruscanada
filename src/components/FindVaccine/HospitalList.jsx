b20e1f13997fc6b3d2f3474f1a4ae101d8805eaf
hector4213 <hectorpwns@gmail.com>
2021-04-03 21:41:30 -0400

    hospital list book button opens modal and passes the hospital selected

--- src/components/FindVaccine/HospitalList.jsx
+++ src/components/FindVaccine/HospitalList.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../redux/ducks/mapSlice';
 
 const HospitalList = ({ hospitals }) => {
+  const dispatch = useDispatch();
   return (
     <ul className="bg-gray-200">
       {hospitals.map((hospital) => (
         <li>
           {hospital.name}
           <button
-            onClick={() => console.log('im clicked!')}
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            onClick={() => dispatch(openModal(hospital))}
+            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="button"
           >
             BOOK NOW
