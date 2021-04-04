b7dd76f32684f7b0f719d7ac8e971222f7f06a9a
hector4213 <hectorpwns@gmail.com>
2021-04-03 21:44:34 -0400

    remove comments add header

--- src/components/FindVaccine/AppointmentModal.jsx
+++ src/components/FindVaccine/AppointmentModal.jsx
@@ -14,7 +14,9 @@ const AppointmentModal = () => {
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/* header */}
             <div className="flex items-start justify-between p-5 border-b border-solid border-gray-200 rounded-t">
-              <h3 className="text-3xl font-semibold">Your Appointment</h3>
+              <h3 className="text-3xl font-semibold">
+                Your Appointment at {selectedHospital.name}
+              </h3>
               <button
                 type="button"
                 className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -25,7 +27,6 @@ const AppointmentModal = () => {
                 </span>
               </button>
             </div>
-            {/* body */}
             <div className="relative p-6 flex-auto">
               <div className="my-4 flex flex-col ">
                 <p>{selectedHospital.name}</p>
@@ -36,7 +37,6 @@ const AppointmentModal = () => {
                 <input type="time" />
               </div>
             </div>
-            {/* footer */}
             <div className="flex items-center justify-end p-6 border-t border-solid border-gray-200 rounded-b">
               <button
                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
