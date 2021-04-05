3c2b08ad7a595815292ea17b4fc89a802b448688
hector4213 <hectorpwns@gmail.com>
2021-04-05 14:06:21 -0400

    change text to sign up

--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -65,7 +65,7 @@ const SignUp = () => {
               type="submit"
               className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
             >
-              Login
+              Sign Up
             </button>
           </div>
         </form>
