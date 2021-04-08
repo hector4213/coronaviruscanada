dbbeb1af50fe1cb8c231f551a4c0fb0433e7f69b
hector4213 <hectorpwns@gmail.com>
2021-04-08 14:30:16 -0400

    create user dashboard

---
+++ src/views/UserDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/UserDashboard/Dashboard';
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default UserDashboard;
