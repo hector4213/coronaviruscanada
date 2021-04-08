6769f293d514484b286d41bb022bee96e293f0c7
hector4213 <hectorpwns@gmail.com>
2021-04-08 14:30:00 -0400

    redirect to user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,14 @@ import Today from './views/Today';
 import Trends from './views/Trends';
 import SignUp from './views/SignUp';
 import Login from './views/Login';
+import UserDashboard from './views/UserDashboard';
 import FindVaccine from './views/FindVaccine';
 import { useAuthSubscription } from './firebase/useAuthSubscription';
 
 const App = () => {
   const dispatch = useDispatch();
   const { apiLastUpdated } = useSelector((state) => state.appData);
-  const { user } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
 
   useAuthSubscription();
 
@@ -52,15 +53,22 @@ const App = () => {
             <Route exact path="/findvaccine">
               <FindVaccine />
             </Route>
+            <Route exact path="/dashboard">
+              <UserDashboard />
+            </Route>
             <Route
               exact
               path="/login"
-              render={() => (user ? <Redirect to="/" /> : <Login />)}
+              render={() =>
+                currentUser ? <Redirect to="/dashboard" /> : <Login />
+              }
             />
             <Route
               exact
               path="/signup"
-              render={() => (user ? <Redirect to="/" /> : <SignUp />)}
+              render={() =>
+                currentUser ? <Redirect to="/dashboard" /> : <SignUp />
+              }
             />
           </Switch>
         </main>
