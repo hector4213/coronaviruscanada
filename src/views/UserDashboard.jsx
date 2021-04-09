91e0341e8dc342e0fdd8a91385baf65f344fb9c9
hector4213 <hectorpwns@gmail.com>
2021-04-09 14:19:56 -0400

    add appointments to userDashboard

--- src/views/UserDashboard.jsx
+++ src/views/UserDashboard.jsx
@@ -1,9 +1,26 @@
-import Dashboard from '../components/UserDashboard/Dashboard';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAppointments } from '../redux/ducks/userSlice';
+import Appointments from '../components/UserDashboard/Appointments';
 
 const UserDashboard = () => {
+  const dispatch = useDispatch();
+  const { appointments, currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (currentUser) {
+      dispatch(getAppointments(currentUser.id));
+    }
+  }, [currentUser]);
+
+  if (!currentUser) {
+    return null;
+  }
+
   return (
     <div>
-      <Dashboard />
+      <h1 className="text-2xl">Hello {currentUser.email}</h1>
+      <Appointments appointments={appointments} />
     </div>
   );
 };
