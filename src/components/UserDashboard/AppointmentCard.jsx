c4c80af9b1d9d13a6ec7ba0efeb42e9c5e0efe5d
hector4213 <hectorpwns@gmail.com>
2021-04-09 14:40:29 -0400

    start delete handler

--- src/components/UserDashboard/AppointmentCard.jsx
+++ src/components/UserDashboard/AppointmentCard.jsx
@@ -2,20 +2,23 @@ import PropTypes from 'prop-types';
 import { GiHealthCapsule } from 'react-icons/gi';
 
 const AppointmentCard = ({ appointment }) => {
+  const { location, time, date, id } = appointment;
+
+  const handleDelete = (apptId) => {
+    console.log(`Deleted appointment with id ${apptId}`);
+  };
+
   return (
     <div className="shadow-lg rounded-2xl w-80 p-4 mb-2 bg-white relative overflow-hidden md:mr-4">
       <GiHealthCapsule className="absolute -right-20 -bottom-8 h-40 w-40 mb-4" />
       <div className="w-4/6 h-full flex flex-col justify-around">
-        <p className="text-gray-800 text-lg font-medium mb-2">
-          {appointment.location}
-        </p>
-        <p className="text-gray-400 text-md">{appointment.date}</p>
-        <p className="text-indigo-500 text-xl font-medium">
-          Time: {appointment.time}
-        </p>
+        <p className="text-gray-800 text-lg font-medium mb-2">{location}</p>
+        <p className="text-gray-400 text-md">{date}</p>
+        <p className="text-indigo-500 text-xl font-medium">Time: {time}</p>
         <button
           className="px-3 py-2 bg-red-500 text-white text-xs font-bold uppercase"
           type="button"
+          onClick={() => handleDelete(id)}
         >
           Cancel
         </button>
