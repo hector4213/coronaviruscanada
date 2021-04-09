019a4669d592c39c012eb761008af1f702f04126
hector4213 <hectorpwns@gmail.com>
2021-04-08 22:52:08 -0400

    fetch user appointments, show user details

--- src/components/UserDashboard/Dashboard.jsx
+++ src/components/UserDashboard/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAppointments } from '../../redux/ducks/userSlice';
 
 const Dashboard = () => {
-  return <div>Im the user dashbaord</div>;
+  const dispatch = useDispatch();
+  const { appointments, currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (currentUser) {
+      dispatch(getAppointments(currentUser.id));
+    }
+  }, [currentUser]);
+
+  if (!currentUser) {
+    return null;
+  }
+  return (
+    <div>
+      <p>Hello {currentUser.email}</p>
+      <p>{appointments.map((appointment) => appointment.location)}</p>
+    </div>
+  );
 };
 
 export default Dashboard;
