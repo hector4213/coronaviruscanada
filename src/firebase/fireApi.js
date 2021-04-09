0765e6d34add55f1663b2af4145fcf1d064c87bf
hector4213 <hectorpwns@gmail.com>
2021-04-08 22:50:14 -0400

    api calls to firestore, fetch user appointments, create a new appointment and add new to state

--- src/firebase/fireApi.js
+++ src/firebase/fireApi.js
@@ -28,9 +28,37 @@ const login = async (email, password) => {
   return loginUser;
 };
 
+const createAppointment = async (obj) => {
+  const db = firebase.firestore();
+
+  try {
+    await db.collection('appointments').add(obj);
+    console.log('success!');
+  } catch (error) {
+    console.log('error occured on appointment fetch', error);
+  }
+};
+
+const fetchAppointments = async (userId) => {
+  const db = firebase.firestore();
+  const appointmentsRef = db
+    .collection('appointments')
+    .where('userId', '==', userId);
+
+  try {
+    const data = await appointmentsRef.get();
+    const appointments = data.docs.map((doc) => doc.data());
+    return appointments;
+  } catch (error) {
+    return console.log('an error occured', error); // handle error?
+  }
+};
+
 export default {
   fetchAppProvinces,
   fetchAppRegions,
   signup,
   login,
+  createAppointment,
+  fetchAppointments,
 };
