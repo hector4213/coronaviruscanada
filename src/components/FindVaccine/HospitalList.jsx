cb852f597276a8f87821b0bf61a4e3b2166f9917
hector4213 <hectorpwns@gmail.com>
2021-04-09 21:41:53 -0400

    style hospital list

--- src/components/FindVaccine/HospitalList.jsx
+++ src/components/FindVaccine/HospitalList.jsx
@@ -5,13 +5,16 @@ import { openModal } from '../../redux/ducks/mapSlice';
 const HospitalList = ({ hospitals }) => {
   const dispatch = useDispatch();
   return (
-    <ul className="bg-gray-200">
+    <ul className="flex flex-col mx-auto w-full justify-center bg-white rounded-lg shadow p-4">
       {hospitals.map((hospital) => (
-        <li key={hospital.key}>
-          {hospital.name}
+        <li
+          key={hospital.key}
+          className="flex flex-col items-center justify-between md:flex-row"
+        >
+          <p className="text-sm">{hospital.name}</p>
           <button
             onClick={() => dispatch(openModal(hospital))}
-            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 sm:flex-shrink"
             type="button"
           >
             BOOK NOW
