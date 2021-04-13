f12c272006025e2543473628f4dc77ff868f3305
hector4213 <hectorpwns@gmail.com>
2021-04-13 14:30:36 -0400

    add loader for Today page

--- src/views/Today.jsx
+++ src/views/Today.jsx
@@ -5,6 +5,7 @@ import { getTodaySummaries } from '../redux/ducks/summarySlice';
 import { getAppProvinces } from '../redux/ducks/infoSlice';
 import ProvinceCard from '../components/ProvinceCard';
 import ProvinceDash from '../components/ProvinceDash';
+import Loader from '../components/Loader';
 
 const Today = () => {
   const { isLoading, today } = useSelector((state) => state.summaries);
@@ -24,34 +25,36 @@ const Today = () => {
     }
   }, []);
 
-  if (isLoading) {
-    return null; //   loader here state is false if data loaded
-  }
-
   return (
-    <div className="container mt-4 mx-auto">
-      <div className="md:grid md:grid-cols-2 lg:grid-cols-3">
-        <Route exact path={`${path}`}>
-          <h1 className="text-3xl col-span-full text-center md:text-4xl font-medium mb-2 p-10">
-            Select a province to view todays stats
-          </h1>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="container mt-4 mx-auto">
+          <div className="md:grid md:grid-cols-2 lg:grid-cols-3">
+            <Route exact path={`${path}`}>
+              <h1 className="text-3xl col-span-full text-center md:text-4xl font-medium mb-2 p-10">
+                Select a province to view todays stats
+              </h1>
+              {provinceData.map((prov) => (
+                <ProvinceCard
+                  key={prov.id}
+                  name={prov.name}
+                  logo={prov.img}
+                  province={today.find((item) => item.province === prov.name)}
+                  path={prov.path}
+                />
+              ))}
+            </Route>
+          </div>
           {provinceData.map((prov) => (
-            <ProvinceCard
-              key={prov.id}
-              name={prov.name}
-              logo={prov.img}
-              province={today.find((item) => item.province === prov.name)}
-              path={prov.path}
-            />
+            <Route path={`${path}${prov.path}`} key={prov.id}>
+              <ProvinceDash code={prov.code} />
+            </Route>
           ))}
-        </Route>
-      </div>
-      {provinceData.map((prov) => (
-        <Route path={`${path}${prov.path}`} key={prov.id}>
-          <ProvinceDash code={prov.code} />
-        </Route>
-      ))}
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
