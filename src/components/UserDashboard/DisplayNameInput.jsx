f2b018f74e4351c14701c8207390db990f98bd87
hector4213 <hectorpwns@gmail.com>
2021-04-13 08:00:30 -0400

    make input component

---
+++ src/components/UserDashboard/DisplayNameInput.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateDisplayName } from '../../redux/ducks/userSlice';
+
+const DisplayNameInput = () => {
+  const [name, setName] = useState('');
+  const dispatch = useDispatch();
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    dispatch(updateDisplayName(name));
+  };
+
+  return (
+    <div className="relative">
+      <label htmlFor="display-name" className="text-gray-700">
+        Update your display name
+        <input
+          type="text"
+          id="display-name"
+          className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          name="display-name"
+          placeholder="Your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <button
+        className="py-2 px-4 mt-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
+        type="submit"
+        onClick={handleUpdate}
+      >
+        Update
+      </button>
+    </div>
+  );
+};
+
+export default DisplayNameInput;
