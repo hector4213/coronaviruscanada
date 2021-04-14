e7c9744d0e6a5f8349b7fa94e4fbf5d0c27003aa
hector4213 <hectorpwns@gmail.com>
2021-04-14 07:08:25 -0400

    use Loader for list

--- src/components/FindVaccine/HospitalList.jsx
+++ src/components/FindVaccine/HospitalList.jsx
@@ -1,29 +1,35 @@
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { openModal } from '../../redux/ducks/mapSlice';
+import Loader from '../Loader';
 
 const HospitalList = ({ hospitals }) => {
+  const { isListLoading } = useSelector((state) => state.map);
   const dispatch = useDispatch();
   return (
     <ul className="flex flex-col mx-auto w-full justify-center bg-white rounded-lg shadow-xl p-4">
       <h1 className="text-center text-2xl p-4 text-blue-500">
         Vaccination sites near you
       </h1>
-      {hospitals.map((hospital) => (
-        <li
-          key={hospital.key}
-          className="flex flex-col items-center justify-between md:flex-row"
-        >
-          <p className="text-sm font-medium">{hospital.name}</p>
-          <button
-            onClick={() => dispatch(openModal(hospital))}
-            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 sm:flex-shrink"
-            type="button"
+      {!isListLoading ? (
+        hospitals.map((hospital) => (
+          <li
+            key={hospital.key}
+            className="flex flex-col items-center justify-between md:flex-row"
           >
-            BOOK NOW
-          </button>
-        </li>
-      ))}
+            <p className="text-sm font-medium">{hospital.name}</p>
+            <button
+              onClick={() => dispatch(openModal(hospital))}
+              className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 sm:flex-shrink"
+              type="button"
+            >
+              BOOK NOW
+            </button>
+          </li>
+        ))
+      ) : (
+        <Loader />
+      )}
     </ul>
   );
 };
