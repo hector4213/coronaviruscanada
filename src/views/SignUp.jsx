8e5d9ffccf12a1a30b9ed2bb5c9c87d41ce270da
hector4213 <hectorpwns@gmail.com>
2021-04-14 07:11:31 -0400

    fix signup layout

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -2,7 +2,7 @@ import SignUpForm from '../components/SignUp/SignUpForm';
 
 const SignUp = () => {
   return (
-    <div>
+    <div className="flex items-center mt-12 h-96 justify-center mx-auto my-auto md:mt-36">
       <SignUpForm />
     </div>
   );
