96b41300ecd8cf9a2dfa30658ae8a370ba83646e
hector4213 <hectorpwns@gmail.com>
2021-04-13 22:32:10 -0400

    add bold font for hospital list

--- src/components/FindVaccine/HospitalList.jsx
+++ src/components/FindVaccine/HospitalList.jsx
@@ -14,7 +14,7 @@ const HospitalList = ({ hospitals }) => {
           key={hospital.key}
           className="flex flex-col items-center justify-between md:flex-row"
         >
-          <p className="text-sm">{hospital.name}</p>
+          <p className="text-sm font-medium">{hospital.name}</p>
           <button
             onClick={() => dispatch(openModal(hospital))}
             className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 sm:flex-shrink"
