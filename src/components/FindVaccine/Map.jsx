4c8d831c1c680cf5ba9751e1537f626e1d3b7039
hector4213 <hectorpwns@gmail.com>
2021-04-13 22:33:31 -0400

    give map fixed height

--- src/components/FindVaccine/Map.jsx
+++ src/components/FindVaccine/Map.jsx
@@ -5,30 +5,32 @@ import UserMarker from './UserMarker';
 
 const Map = ({ userLocation, mapResults }) => {
   return (
-    <MapContainer
-      className="h-full w-full"
-      center={userLocation}
-      zoom={10}
-      scrollWheelZoom
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">Mapbox</a> contributors'
-        url={`https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX}`}
-      />
-      <Marker position={userLocation}>
-        <Popup>You are here :)</Popup>
-      </Marker>
-      <UserMarker location={userLocation} />
-      {mapResults.length < 1
-        ? null
-        : mapResults.map((hospital) => (
-            <HospitalMarker
-              key={hospital.key}
-              hospital={hospital}
-              position={[hospital.fields.lat, hospital.fields.lng]}
-            />
-          ))}
-    </MapContainer>
+    <div className="h-96 flex-1">
+      <MapContainer
+        className="h-full w-full"
+        center={userLocation}
+        zoom={11}
+        scrollWheelZoom
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">Mapbox</a> contributors'
+          url={`https://api.mapbox.com/styles/v1/hector4213/ckmnso0et2kwu17nry64cn5bo/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX}`}
+        />
+        <Marker position={userLocation}>
+          <Popup>You are here :)</Popup>
+        </Marker>
+        <UserMarker location={userLocation} />
+        {mapResults.length < 1
+          ? null
+          : mapResults.map((hospital) => (
+              <HospitalMarker
+                key={hospital.key}
+                hospital={hospital}
+                position={[hospital.fields.lat, hospital.fields.lng]}
+              />
+            ))}
+      </MapContainer>
+    </div>
   );
 };
 
