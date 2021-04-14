ba2cd112270bbe4b44ce6cb79ea2e650d3937ae5
hector4213 <hectorpwns@gmail.com>
2021-04-14 07:11:16 -0400

    fix login layout

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,7 +2,7 @@ import LoginForm from '../components/Login/LoginForm';
 
 const Login = () => {
   return (
-    <div>
+    <div className="flex items-center mt-12 h-96 justify-center mx-auto my-auto md:mt-36">
       <LoginForm />
     </div>
   );
