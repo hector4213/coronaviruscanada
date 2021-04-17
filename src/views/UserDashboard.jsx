2146709d1550f828d64bee4889a6f4211bc50d08
hector4213 <hectorpwns@gmail.com>
2021-04-16 22:30:36 -0400

    show human date, fix layout

--- src/views/UserDashboard.jsx
+++ src/views/UserDashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { format } from 'date-fns';
 import { getAppointments } from '../redux/ducks/userSlice';
 import Appointments from '../components/UserDashboard/Appointments';
 import DisplayNameInput from '../components/UserDashboard/DisplayNameInput';
@@ -16,16 +17,21 @@ const UserDashboard = () => {
 
   if (!currentUser) {
     return null;
-  }
+  } //  TODO : Do leader
+
+  const humanDate = () => {
+    const today = new Date();
+    return format(today, 'PPPP');
+  };
 
   return (
     <div className="p-4 flex-1">
       <div className="md:grid grid-cols-2 grid-rows-2 gap-4">
-        <div className="flex justify-center items-center bg-white rounded-lg shadow-xl">
+        <div className="flex flex-col justify-center items-center bg-white rounded-lg shadow-xl">
           <h1 className="text-3xl text-center md:text-4xl font-medium mb-2 block">
             Welcome, {currentUser.displayName || currentUser.email}, its
-            <span className="text-sm">{new Date().toString()}</span>
           </h1>
+          <span className="text-lg text-indigo-600">{humanDate()}</span>
         </div>
         <div className="flex justify-center items-center bg-white rounded-lg shadow-xl">
           <DisplayNameInput />
