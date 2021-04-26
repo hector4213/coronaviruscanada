7015a937583be7594c63f10a9875636cf4a18c81
hector4213 <hectorpwns@gmail.com>
2021-04-26 01:40:18 -0400

    add default field for user appointment in docs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -34,6 +34,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       await userRef.set({
         displayName,
         email,
+        appts: 0,
         ...additionalData,
       });
     } catch (error) {
