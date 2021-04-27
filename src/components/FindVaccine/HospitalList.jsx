b0852a4ebdbf21d899b4b652a998b54594e5243d
hector4213 <hectorpwns@gmail.com>
2021-04-26 22:46:56 -0400

    style hospital list

--- src/components/FindVaccine/HospitalList.jsx
+++ src/components/FindVaccine/HospitalList.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import { FaSearchLocation } from 'react-icons/fa';
 import { openModal } from '../../redux/ducks/mapSlice';
 import Loader from '../Loader';
 
@@ -7,15 +8,20 @@ const HospitalList = ({ hospitals }) => {
   const { isListLoading } = useSelector((state) => state.map);
   const dispatch = useDispatch();
   return (
-    <ul className="flex flex-col mx-auto w-full justify-center bg-white rounded-lg shadow-xl p-4">
-      <h1 className="text-center text-2xl p-4 text-blue-500">
-        Vaccination sites near you
-      </h1>
+    <ul className="flex flex-col mx-auto w-full justify-center bg-white rounded-lg shadow-xl">
+      <div className="flex flex-col mb-10 items-center justify-center w-full md:flex-row">
+        <h1 className="mx-4 text-center text-4xl font-medium p-4 text-blue-500">
+          Vaccination sites near you
+        </h1>
+        <div className="flex-shrink my-auto p-4">
+          <FaSearchLocation size={70} />
+        </div>
+      </div>
       {!isListLoading ? (
         hospitals.map((hospital) => (
           <li
             key={hospital.key}
-            className="flex flex-col items-center justify-between md:flex-row"
+            className="flex flex-col items-center justify-between md:flex-row mx-10"
           >
             <p className="text-sm font-medium">{hospital.name}</p>
             <button
