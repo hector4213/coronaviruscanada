4e5db47828deafae6df9be8be52f37857e3b63c6
hector4213 <hectorpwns@gmail.com>
2021-04-27 13:24:26 -0400

    change to Link tag

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,9 +17,9 @@ const Header = () => {
         <div className="max-w-7xl mx-auto px-8">
           <div className="flex items-center justify-between h-16">
             <div className=" flex items-center">
-              <a className="flex-shrink-0" href="/">
+              <Link className="flex-shrink-0" to="/">
                 <img className="h-12 w-12" src={Logo} alt="headericon" />
-              </a>
+              </Link>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <Link
