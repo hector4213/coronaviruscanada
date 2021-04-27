2a111ff1f886bc300c7ae26317fbfe9e6f913b96
hector4213 <hectorpwns@gmail.com>
2021-04-27 13:51:02 -0400

    space display name input

--- src/components/UserDashboard/DisplayNameInput.jsx
+++ src/components/UserDashboard/DisplayNameInput.jsx
@@ -14,18 +14,22 @@ const DisplayNameInput = () => {
 
   return (
     <div className="flex flex-col mt-4">
-      <label htmlFor="display-name" className="text-gray-700 text-center ">
-        Update your display name
-        <input
-          type="text"
-          id="display-name"
-          className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-          name="display-name"
-          placeholder="Your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
+      <div className="px-8">
+        <label htmlFor="displayName" className="text-gray-700">
+          <p className="w-full my-4 font-light text-center mx-auto ">
+            Update your display name
+          </p>
+          <input
+            type="text"
+            id="displayName"
+            className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+            name="displayName"
+            placeholder="Your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
       <div className="mx-auto">
         <button
           className="py-2 px-4 mt-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg md:w-48"
