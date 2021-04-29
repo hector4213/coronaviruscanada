fd80322af00035b59279843c12c0352b6f219dba
hector4213 <hectorpwns@gmail.com>
2021-04-28 21:22:21 -0400

    show nothing if user not logged in

--- src/views/UserDashboard.jsx
+++ src/views/UserDashboard.jsx
@@ -15,10 +15,6 @@ const UserDashboard = () => {
     }
   }, [currentUser]);
 
-  if (!currentUser) {
-    return null;
-  } //  TODO : Do leader
-
   const humanDate = () => {
     const today = new Date();
     return format(today, 'PPPP');
@@ -26,21 +22,25 @@ const UserDashboard = () => {
 
   return (
     <div className="p-4 h-full">
-      <div className="md:grid grid-cols-2 grid-rows-2 gap-4 h-full">
-        <div className="flex flex-col justify-center items-center bg-white rounded-lg shadow-xl">
-          <h1 className="text-xl font-thin p-4 text-center md:text-4xl md:p-8 mb-2 block">
-            Welcome {currentUser.displayName || currentUser.email}, its
-          </h1>
-          <span className="text-lg text-indigo-600">{humanDate()}</span>
+      {currentUser ? (
+        <div className="md:grid grid-cols-2 grid-rows-2 gap-4 h-full">
+          <div className="flex flex-col justify-center items-center bg-white rounded-lg shadow-xl">
+            <h1 className="text-xl font-thin p-4 text-center md:text-4xl md:p-8 mb-2 block">
+              Welcome {currentUser.displayName || currentUser.email}, its
+            </h1>
+            <span className="text-lg text-indigo-600">{humanDate()}</span>
+          </div>
+          <div className="flex justify-center items-center bg-white rounded-lg shadow-xl">
+            <DisplayNameInput />
+          </div>
+          <div className="flex justify-center items-center bg-white rounded-lg shadow-xl md:row-span-1 col-span-2">
+            <Appointments appointments={appointments} />
+          </div>
+          <div className="bg-white rounded-lg shadow-xl" />
         </div>
-        <div className="flex justify-center items-center bg-white rounded-lg shadow-xl">
-          <DisplayNameInput />
-        </div>
-        <div className="flex justify-center items-center bg-white rounded-lg shadow-xl md:row-span-1 col-span-2">
-          <Appointments appointments={appointments} />
-        </div>
-        <div className="bg-white rounded-lg shadow-xl" />
-      </div>
+      ) : (
+        'You need to be logged in to view your dashboard'
+      )}
     </div>
   );
 };
